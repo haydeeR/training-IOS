bcd4d6785c2563608216598a4585e91ccc4531d6
Haydee Rodriguez <hrodriguez@nearsoft.com>
2018-04-03 13:36:52 -0500

    Change nib inside scene

---
+++ reachabilityTest/reachabilityTest/OfflineNibViewController.swift
@@ -0,0 +1,24 @@
+//
+//  OfflineNibViewController.swift
+//  reachabilityTest
+//
+//  Created by Haydee Rodriguez on 4/3/18.
+//  Copyright © 2018 Haydee Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class OfflineNibViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
