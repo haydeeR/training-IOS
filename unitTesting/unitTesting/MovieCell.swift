ab60df9d12b1c1e7a2645897a8a7ce2010fb536e
Haydee Rodriguez <hrodriguez@nsl-hrodriguezs-MacBook-Pro.local>
2019-04-12 14:40:41 -0500

    Add new project with CoreData

---
+++ unitTesting/unitTesting/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  unitTesting
+//
+//  Created by Haydee Rodriguez on 2/5/19.
+//  Copyright © 2019 Haydee Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
